3a418f0d007134098c8eb1ce56e69875317bd16f
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-08-31 15:20:35 -0500

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ConfigComponent } from './components/config/config.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
